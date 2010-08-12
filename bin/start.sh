349261665089dcbb249cacf2e576e84a1348d39c
Mark Bucciarelli <mark@x30.crosscutmedia.com>
2010-08-12 17:28:09 -0400

    log some time, tweak the task list, and write a shell script that makes it easier to track time.  TODO: stop.sh

---
+++ bin/start.sh
@@ -0,0 +1,27 @@
+#! /bin/sh -e
+#
+# Add a new entry to ../dat/act.txt with current start time.
+#
+
+
+usage="$0 <taskid> [<username>]"
+
+# Get task id and optionally username from command line args.
+[ "x$1" = x ] && echo $usage && exit 1
+taskid=$1
+user=$(whoami)
+if [ "x$2" != "x" ]; then user=$2; fi
+
+# Make sure we can find file holding actual data.
+fn=./dat/act.txt
+if [ ! -f $fn ]; then 
+	echo "ERROR: run from parent directory of ./dat/"
+	exit 1
+fi
+
+dt=$(date +%F)
+tm=$(date +%T)
+
+cat >> $fn << EOF
+$taskid	$user	$dt	$tm
+EOF
